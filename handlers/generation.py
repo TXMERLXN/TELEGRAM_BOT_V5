e6c81e311d01e95d67d7c96d1f8ef28d8400fa75
TXMERLXN <gamer.meximas@gmail.com>
2025-01-13 16:00:10 +0300

    fix: удалена лишняя инициализация клиентов в setup_api

--- handlers/generation.py
+++ handlers/generation.py
@@ -36,7 +36,6 @@ async def setup_api():
         workflow_id=os.getenv('RUNNINGHUB_WORKFLOW_PRODUCT'),
         max_tasks=1
     )
-    task_queue.initialize_clients()
 
 logger = logging.getLogger(__name__)
 
