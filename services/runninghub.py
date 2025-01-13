53bfd0319de881a17e94e7eb3a5429e06247d087
TXMERLXN <gamer.meximas@gmail.com>
2025-01-13 15:21:06 +0300

    fix: возвращены корректные ID узлов: #2 для продукта и #32 для фона

--- services/runninghub.py
+++ services/runninghub.py
@@ -141,12 +141,12 @@ class RunningHubAPI:
                     "apiKey": self.api_key,
                     "nodeInfoList": [
                         {
-                            "nodeId": "4",  # ID узла для продукта
+                            "nodeId": "2",  # ID узла для продукта
                             "fieldName": "image",
                             "fieldValue": product_url
                         },
                         {
-                            "nodeId": "7",  # ID узла для фона
+                            "nodeId": "32",  # ID узла для фона
                             "fieldName": "image",
                             "fieldValue": background_url
                         }
