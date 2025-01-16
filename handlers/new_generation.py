ade20d0620f180e40d6f3a11cd959525b6fa4139
TXMERLXN <gamer.meximas@gmail.com>
2025-01-16 15:31:18 +0300

    Fix CallbackQuery import in new_generation.py

--- handlers/new_generation.py
+++ handlers/new_generation.py
@@ -3,7 +3,8 @@ import logging
 from aiogram import Bot, F, Router
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message, CallbackQuery, URLInputFile
+from aiogram.types import Message, URLInputFile
+from aiogram.types import CallbackQuery
 from aiogram.fsm.state import State, StatesGroup
 
 from services.integration import integration_service
